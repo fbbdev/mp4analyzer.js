d1b9d80399ce149051c80fc2396d97bb84ad9900
Fabio Massaioli <fabio.massaioli@gmail.com>
2013-10-25 20:11:45 +0200

    Replace file with blob

--- src/analyze.js
+++ src/analyze.js
@@ -1,19 +1,19 @@
-MP4.analyze = function(file, callback) {
-  if (!(file instanceof File)) throw new TypeError("Invalid argument type");
-  if (file.type != "video/mp4" && file.type != "video/quicktime") {
-    throw new TypeError("Input file format is not mp4/mov");
+MP4.analyze = function(blob, callback) {
+  if (!(blob instanceof Blob)) throw new TypeError("Invalid argument type");
+  if (blob.type != "video/mp4" && blob.type != "video/quicktime") {
+    throw new TypeError("Input data format is not mp4/mov");
   }
 
   var ab = new ArrayBuffer(8);
   var dv = new DataView(ab);
   var atom = new MP4.Atoms.Atom(dv);
 
-  atom.size = MP4.Atoms.Atom.HeaderSize + file.size;
+  atom.size = MP4.Atoms.Atom.HeaderSize + blob.size;
   atom.type = 'root';
   atom.dataSize = -8;
   atom.parsed = true;
 
-  MP4.Atoms.visitChildren(atom, file, new MP4.Context(), function(context) {
+  MP4.Atoms.visitChildren(atom, blob, new MP4.Context(), function(context) {
     callback(context.result());
   });
 }
